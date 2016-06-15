835e92ce98b56d1187d0006b06ef59893318c557
jivie <joseph@lightningkite.com>
2016-06-15 09:49:29 -0600

    Files re-added

---
+++ src/main/java/com/lightningkite/kotlin/anko/networking/List.ext.kt
@@ -0,0 +1,31 @@
+package com.lightningkite.kotlin.anko.networking
+
+import android.content.Context
+import com.lightningkite.kotlin.files.child
+import com.lightningkite.kotlin.networking.load
+import com.lightningkite.kotlin.networking.save
+import java.lang.reflect.Type
+
+/**
+ * Created by jivie on 3/29/16.
+ */
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun <E> List<E>.save(context: Context, name: String) {
+    val folder = context.filesDir.child("lists")
+    val file = folder.child(name + ".json")
+    save(file)
+}
+
+inline fun <reified E : Any> MutableList<E>.load(context: Context, name: String) {
+    val folder = context.filesDir.child("lists")
+    val file = folder.child(name + ".json")
+    load(file)
+}
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun <E : Any> MutableList<E>.load(context: Context, name: String, type: Type) {
+    val folder = context.filesDir.child("lists")
+    val file = folder.child(name + ".json")
+    load(file, type)
+}
---
+++ src/main/java/com/lightningkite/kotlin/anko/networking/Uri.ext.kt
@@ -0,0 +1,23 @@
+package com.lightningkite.kotlin.anko.networking
+
+import android.content.ContentResolver
+import android.net.Uri
+import com.lightningkite.kotlin.anko.files.fileSize
+import com.lightningkite.kotlin.networking.NetBody
+import com.lightningkite.kotlin.networking.NetContentType
+
+/**
+ * Created by jivie on 6/2/16.
+ */
+
+fun Uri.toNetBody(resolver: ContentResolver): NetBody.StreamBody {
+    val type = resolver.getType(this) ?: throw IllegalArgumentException()
+    println(type)
+    val size = resolver.fileSize(this) ?: throw IllegalArgumentException()
+    println("type: $type, size: $size")
+    return NetBody.StreamBody(
+            NetContentType(type),
+            size,
+            resolver.openInputStream(this)
+    )
+}
