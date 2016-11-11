9e6ab96cd10c404042787165a1dd9a3e9faf8604
Joseph Ivie <josephivie@gmail.com>
2016-11-10 22:02:47 -0700

    Major updates to the example, animations, and networking

--- src/main/java/com/lightningkite/kotlin/anko/networking/Uri.ext.kt
+++ src/main/java/com/lightningkite/kotlin/anko/networking/Uri.ext.kt
@@ -2,30 +2,15 @@ package com.lightningkite.kotlin.anko.networking
 
 import android.content.ContentResolver
 import android.net.Uri
-import com.lightningkite.kotlin.anko.files.fileSize
-import com.lightningkite.kotlin.networking.NetBody
-import com.lightningkite.kotlin.networking.NetContentType
+import com.lightningkite.kotlin.stream.toByteArray
+import okhttp3.MediaType
+import okhttp3.RequestBody
 
 /**
  * Created by jivie on 6/2/16.
  */
 
-fun Uri.toNetBody(resolver: ContentResolver): NetBody.StreamBody {
+fun Uri.toRequestBody(resolver: ContentResolver): RequestBody {
     val type = resolver.getType(this) ?: pathSegments.lastOrNull()?.split('.')?.lastOrNull() ?: throw IllegalArgumentException()
-    val size = resolver.fileSize(this) ?: throw IllegalArgumentException()
-    return NetBody.StreamBody(
-            NetContentType.fromString(type),
-            size,
-            resolver.openInputStream(this)
-    )
-}
-
-fun NetBody.Companion.fromUri(uri: Uri, resolver: ContentResolver): NetBody {
-    val type = resolver.getType(uri) ?: uri.pathSegments.lastOrNull()?.split('.')?.lastOrNull() ?: throw IllegalArgumentException()
-    val size = resolver.fileSize(uri) ?: throw IllegalArgumentException()
-    return NetBody.StreamBody(
-            NetContentType.fromString(type),
-            size,
-            resolver.openInputStream(uri)
-    )
+    return okhttp3.RequestBody.create(MediaType.parse(type), resolver.openInputStream(this).toByteArray())
 }
