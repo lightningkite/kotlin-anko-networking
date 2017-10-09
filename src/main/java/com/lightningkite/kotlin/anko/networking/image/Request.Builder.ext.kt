ef28f138ebaeab1d4a1a41ef45f84413882ed518
Joseph Ivie <josephivie@gmail.com>
2017-10-09 12:53:16 -0600

    Bucketizing has been refactored / cleaned up Clear data on scale functionality added Offline data loading has been tested working

--- src/main/java/com/lightningkite/kotlin/anko/networking/image/Request.Builder.ext.kt
+++ src/main/java/com/lightningkite/kotlin/anko/networking/image/Request.Builder.ext.kt
@@ -12,24 +12,24 @@ import okhttp3.Request
 import java.io.File
 
 fun Request.Builder.lambdaBitmap(options: BitmapFactory.Options = BitmapFactory.Options()) = lambda<Bitmap> {
-    BitmapFactory.decodeStream(it.body().byteStream(), null, options)
+    BitmapFactory.decodeStream(it.body()!!.byteStream(), null, options)
 }
 
 fun Request.Builder.lambdaBitmap(minBytes: Long) = lambda<Bitmap> {
     val options = BitmapFactory.Options().apply {
-        inSampleSize = ImageUtils.calculateInSampleSize(it.body().contentLength(), minBytes)
+        inSampleSize = ImageUtils.calculateInSampleSize(it.body()!!.contentLength(), minBytes)
     }
-    BitmapFactory.decodeStream(it.body().byteStream(), null, options)
+    BitmapFactory.decodeStream(it.body()!!.byteStream(), null, options)
 }
 
 fun Request.Builder.lambdaBitmapExif(context: Context, minBytes: Long) = lambda<Bitmap> {
     val tempFile = File.createTempFile("image", "jpg", context.cacheDir)
-    it.body().byteStream().writeToFile(tempFile)
+    it.body()!!.byteStream().writeToFile(tempFile)
     context.getBitmapFromUri(Uri.fromFile(tempFile), minBytes)!!
 }
 
 fun Request.Builder.lambdaBitmapExif(context: Context, maxWidth: Int = 2048, maxHeight: Int = 2048) = lambda<Bitmap> {
     val tempFile = File.createTempFile("image", "jpg", context.cacheDir)
-    it.body().byteStream().writeToFile(tempFile)
+    it.body()!!.byteStream().writeToFile(tempFile)
     context.getBitmapFromUri(Uri.fromFile(tempFile), maxWidth, maxHeight)!!
 }
